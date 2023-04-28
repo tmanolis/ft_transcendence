7397141057e5bea88dac1fe10023ea1585fd8489
Jas0n Huang <49294117+JAS0NHUANG@users.noreply.github.com>
2023-04-28 21:19:14 +0000

    add SSL cert and key

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+
+// SSL cert
+const keyFile  = fs.readFileSync('/etc/ssl/private.key');
+const certFile  = fs.readFileSync('/etc/ssl/certificate.crt');
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: keyFile,
+      cert: certFile,
+    }
+  });
   await app.listen(3000);
 }
 bootstrap();
