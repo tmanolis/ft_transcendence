d56a028ceb70d27d79fa55f4366298b116092629
Jas0nhuang <49294117+JAS0NHUANG@users.noreply.github.com>
2023-05-01 20:59:52 +0200

    Made the project esier to run
    - Update README.
    - Comment out the SSL HTTPS parts.
    - Update docker-compose.yml file.
    - Remove devops folder.
    
    It should be easy to run this project on any machine now. ^_^
    With docker installed of course.

--- Makefile
+++ Makefile
@@ -1,16 +1,16 @@
 all:
-	mkdir -p ~/transcendence/database
-	docker-compose -f ./devops/docker-compose.yml up --build -d
+	mkdir ./database
+	docker compose up --build -d
 
 down:
-	docker-compose -f ./devops/docker-compose.yml down
+	docker compose down
 
 fclean:	down
 	docker rmi -f $$(docker images -aq)
 	docker system prune --all --force --volumes
 	docker network prune --force
 	docker volume prune --force
-	rm -rf ~/transcendence/database
+	rm -rf ./database
 
 .PHONY:	all down fclean
 
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,24 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import * as fs from 'fs';
+// import * as fs from 'fs';
 
 // SSL cert
-const keyFile  = fs.readFileSync('/etc/ssl/private.key');
-const certFile  = fs.readFileSync('/etc/ssl/certificate.crt');
+// const keyFile  = fs.readFileSync('/etc/ssl/private.key');
+// const certFile  = fs.readFileSync('/etc/ssl/certificate.crt');
 
 
 async function bootstrap() {
 
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions: {
-      // https SSL key and cert
-      key: keyFile,
-      cert: certFile,
-    }
-  });
+  const app = await NestFactory.create(AppModule
+    // , { httpsOptions: { key: keyFile, cert: certFile, } }
+	);
 
-  
   // swagger config and integration
   const swaggerConfig = new DocumentBuilder()
     .setTitle('ft_transcendence')
