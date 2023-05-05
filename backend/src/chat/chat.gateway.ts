871c82425799d0389128680ccda15fd4f9abd52a
Jas0nhuang <49294117+JAS0NHUANG@users.noreply.github.com>
2023-05-05 19:05:01 +0200

    Set CORS as true for chat gateway.

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -1,19 +1,16 @@
 import { SubscribeMessage, WebSocketGateway, MessageBody, WebSocketServer } from '@nestjs/websockets';
 import { Server } from 'socket.io';
 
-@WebSocketGateway()
+@WebSocketGateway({cors:true})
 export class ChatGateway {
   @WebSocketServer()
   server: Server;
 
   @SubscribeMessage('message')
-  handleMessage(@MessageBody() body: any, client: any, payload: any): string {
-    console.log(payload);
-    console.log(body);
-	this.server.emit('onMessage',  {
-		msg: "hello",
-		content: body,
-	})
-    return 'Hello world!';
+  handleMessage(@MessageBody() body: any, client: any, payload: any) {
+    this.server.emit('onMessage',  {
+      msg: "hello",
+      content: body,
+    })
   }
 }
