d9ba1d59a18640643d79312dc6a32d151532a705
Amber van Bree <avan-bre@students.42.fr>
2023-05-22 19:11:13 +0200

    connecting 42 user to database, next steps: id should be uuid and not the id of 42, check if user exists is not done yet (it doesn't log user info correctly), see if dto is necessary or it's ok with req, implement jwt tokens and guards

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'nestjs-prisma';
 import { AuthDto } from './dto/auth.dto';
+import * as argon from 'argon2'
 
 @Injectable()
 export class AuthService {
@@ -13,7 +14,32 @@ export class AuthService {
 	// }
 	async handle42Login(res: any, req: any): Promise<string> {
 		res.cookie('access_token', req.user).redirect('/hello');
-		console.log(req.user);
+		
+		const hash = await argon.hash(req.user.accessToken);
+		
+		const user = await this.prisma.user.findUnique({
+			where: {
+				id: req.user.id,
+			},
+		})
+		
+		if (!user){
+			try {
+				const user = await this.prisma.user.create({
+					data: {
+						id: req.user.id,
+						email: req.user.email,
+						userName: req.user.login,
+						avatar: req.user.image,
+						isFourtyTwoStudent: true,
+						hash,
+					}
+				})
+			}catch (error) {
+				throw error;
+			}
+		}
+		console.log(user);
 		return 'OK!';
 	  }
 }
