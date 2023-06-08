3c3bd2ea3c9e2636feda6ba4a36a9cf78733e70c
JAS0NHUANG <49294117+JAS0NHUANG@users.noreply.github.com>
2023-06-08 17:59:29 +0200

    enable cors
    - enable cors
    - clean up the game.gateway. Will integrate the game.service latter

--- backend/src/game/game.gateway.ts
+++ backend/src/game/game.gateway.ts
@@ -4,38 +4,55 @@ import {
   MessageBody,
   WebSocketServer,
   ConnectedSocket,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
   WsResponse,
   WsException,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway({ cors: true })
+// import { GameService } from './game.service';
+
+@WebSocketGateway({
+  cors: {
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:5173",
+      "http://jas0nhuang.eu.org:3000",
+      "http://jas0nhuang.eu.org:5173",
+    ],
+  },
+  namespace: 'game'
+})
 export class GameGateway {
+  constructor(/*private readonly gameService: GameService*/) {}
+
   @WebSocketServer()
   server: Server;
 
-  async handleConnection(client: Socket, payload: any) {
-    console.log('client auth: ', client.handshake.headers.authorization);
-    console.log('client headers: ', client.handshake.headers);
-    if (!client.handshake.headers.authorization) {
-      client.disconnect();
-      console.log('cannot connect!!!');
-    }
-    console.log(client.id);
+  /****************************************************************************/
+  /* handle connection                                                        */
+  /****************************************************************************/
+  async handleConnection(client: Socket) {
+    // await this.gameService.userConnect(client, this.server);
   }
 
-  leftPlayerPosition = { x: 0, y: 0 };
-  rightPlayerPosition = { x: 580, y: 0 };
+  /****************************************************************************/
+  /* handle disconnection                                                     */
+  /****************************************************************************/
+  async handleDisconnect(client: Socket) {
+    console.log(client.id, ' disconnected.');
+  }
 
+  /****************************************************************************/
+  /* player movements                                                         */
+  /****************************************************************************/
   @SubscribeMessage('leftPlayerMove')
   handleLeftPlayerMove(
     @MessageBody() body: any,
     @ConnectedSocket() client: any,
   ) {
-    if (body > 360) body = 360;
-    const newX = 0 + body * (2 / 9);
-    this.leftPlayerPosition = { x: newX, y: body };
-    this.server.emit('leftPlayerPosition', { x: newX, y: body });
+    // this.gameService.throttledPlayerMove(this.server, body, client, 'left');
   }
 
   @SubscribeMessage('rightPlayerMove')
@@ -43,58 +60,21 @@ export class GameGateway {
     @MessageBody() body: any,
     @ConnectedSocket() client: any,
   ) {
-    if (body > 360) body = 360;
-    const newX = 580 + body * (2 / 9);
-    this.rightPlayerPosition = { x: newX, y: body };
-    this.server.emit('rightPlayerPosition', { x: newX, y: body });
+    // this.gameService.throttledPlayerMove(this.server, body, client, 'right');
   }
 
-  @SubscribeMessage('gameStart')
-  handleGameStart(@MessageBody() body: any) {
-    console.log(body);
-    let x = body.x;
-    let y = body.y;
-    let randX = Math.floor(Math.floor((Math.random() - 0.5) * 100) * 0.1);
-    const randY = Math.floor(Math.floor((Math.random() - 0.5) * 100) * 0.1);
-    if (randX < 3 && randX >= 0) {
-      randX += 3;
-    }
-    if (randX > -3 && randX <= 0) {
-      randX -= 3;
-    }
-    const direction = { x: randX, y: randY };
+  @SubscribeMessage('startGame')
+  handleStartGame(
+    @MessageBody() body: any,
+    @ConnectedSocket() client: any,
+  ) {
+    // this.gameService.gameLoop(this.server, body, client);
+  }
 
-    const gameInterval = setInterval(() => {
-      x += direction.x;
-      y += direction.y;
-      console.log(direction.x, direction.y);
-      this.server.emit('ballPosition', { x: x, y: y });
-      console.log(this.leftPlayerPosition);
-      if (y >= 345) {
-        direction.x;
-        direction.y *= -1;
-      }
-      if (y <= 15) {
-        direction.y *= -1;
-      }
-      if (
-        x <= this.leftPlayerPosition.x &&
-        y <= this.leftPlayerPosition.y + 50 &&
-        y >= this.leftPlayerPosition.y - 50
-      ) {
-        if (y > this.leftPlayerPosition) direction.y += 5;
-        else direction.y -= 5;
-
-        direction.x *= -1;
-      }
-      console.log('rightppos:', this.rightPlayerPosition);
-      if (x >= this.rightPlayerPosition.x) {
-        direction.x *= -1;
-      }
-      if (x > 720 || x < 0) {
-        clearInterval(gameInterval);
-        this.server.emit('gameStop', null);
-      }
-    }, 1000 / 30);
+  @SubscribeMessage('canvasOffsetTop')
+  handleCanvasOffsetTop(
+    @MessageBody() body: number
+  ) {
+    // this.gameService.setCanvasOffsetTop(body);
   }
 }
