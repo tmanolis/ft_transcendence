e9ba41f58dcc1a31117905babfb4b0b4bd6381d5
jas0nhuang <49294117+JAS0NHUANG@users.noreply.github.com>
2023-08-10 15:28:20 +0200

    WebSocket Gateway reorganization
    - remove chat/game seperate gateways/modules
    - use a generic "socket" module
    
    * See README.md in "backend/src/socket" for more details.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,27 +3,26 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from 'nestjs-prisma';
 
 import { HelloModule } from './hello/hello.module';
-import { ChatModule } from './chat/chat.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { GameGateway } from './game/game.gateway';
 import { AuthService } from './auth/auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 import { TwoFA } from './auth/strategy';
+import { SocketModule } from './socket/socket.module';
 
 @Module({
   imports: [
     HelloModule,
     PrismaModule.forRoot(),
     ConfigModule.forRoot({ isGlobal: true }),
-    ChatModule,
     UserModule,
     AuthModule,
     JwtModule,
+    SocketModule,
   ],
-  providers: [GameGateway, AuthService, UserService, TwoFA],
+  providers: [AuthService, UserService, TwoFA],
   controllers: [UserController],
 })
 export class AppModule {}
---
+++ backend/src/socket/socket.gateway.ts
@@ -0,0 +1,67 @@
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  MessageBody,
+  WebSocketServer,
+  ConnectedSocket,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WsResponse,
+  WsException,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io'
+
+// Will implement latter 
+// import { GameService } from './game/game.service';
+// import { ChatService } from './chat/chat.service';
+
+@WebSocketGateway({
+  cors: {
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:8080',
+    ],
+  }
+})
+export class SocketGateway implements OnGatewayConnection{
+  @WebSocketServer()
+  server: Server;
+
+  /****************************************************************************/
+  /* handle connection/disconnection                                          */
+  /****************************************************************************/
+  async handleConnection(client: Socket) {
+    console.log(client.id, ' connected to generic socket. ^_^');
+    // Will implement latter
+    // await this.gameService.userConnect(client, this.server);
+  }
+
+  async handleDisconnect(client: Socket) {
+    console.log(client.id, ' disconnected from generic socket. 0.0');
+  }
+  
+  /****************************************************************************/
+  /* listen/emit socket event                                                 */
+  /****************************************************************************/
+
+  /****************************************************************************/
+  /* GAME                                                                     */
+  /****************************************************************************/
+  @SubscribeMessage('movePaddle')
+  handleMovePaddle(client: Socket, payload: Object ): Object {
+    console.log(payload);
+    console.log("Paddle movinnnnn!!!");
+    return {"event": "player paddle move", "socketID": client.id};
+
+  }
+
+  /****************************************************************************/
+  /* CHAT                                                                     */
+  /****************************************************************************/
+  @SubscribeMessage('message')
+  handleMessageReceived(client: Socket, payload: Object ): Object {
+    console.log(payload);
+    console.log("Message received!!!");
+    return {"event": "player message receivedt ", "socketID": client.id};
+  }
+}
