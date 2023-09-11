81b8784c92d5b6950fda9a5e4afafaa63f051b2c
Amber van Bree <avan-bre@students.42.fr>
2023-09-11 18:04:43 +0200

    invite functions done, not tested well yet

--- App/src/App.tsx
+++ App/src/App.tsx
@@ -2,12 +2,14 @@ import Authentification from "./pages/Authentification";
 import Pong from "./pages/Pong";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React from "react";
+import Hello from "./pages/Hello";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={React.createElement(Authentification)} />
+				<Route path="/hello" element={React.createElement(Hello)} />
         <Route path="/pong" element={React.createElement(Pong)} />
       </Routes>
     </Router>
