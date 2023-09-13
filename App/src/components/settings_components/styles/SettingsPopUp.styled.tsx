3cbc857bf428f0e6208313f3f375185456df3774
Tiffany MANOLIS <tiffany.mano@gmail.com>
2023-09-13 18:28:53 +0200

    settings structure basics

---
+++ App/src/components/settings_components/styles/SettingsPopUp.styled.tsx
@@ -0,0 +1,11 @@
+// /* Pop up */
+
+// position: absolute;
+// width: 398px;
+// height: 521px;
+// left: calc(50% - 398px/2);
+// top: calc(50% - 521px/2 + 9.5px);
+
+// background: #FFFFFF;
+// border: 1px solid #FFFFFF;
+// box-shadow: inset -5px -5px 0px #727272;
--- App/src/pages/Settings.tsx
+++ App/src/pages/Settings.tsx
@@ -1,9 +1,11 @@
+import SettingsPopUp from "../components/settings_components/SettingsPopUp";
 import Landing from "../pages/Landing";
 
 const Settings = () => {
     return (
         <>
             <Landing />
+            <SettingsPopUp />
        </>
     );
 }
