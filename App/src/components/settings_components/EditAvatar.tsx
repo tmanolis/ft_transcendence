5f80a1e76ae7951a896c6df7152fa635d40a3c30
Tiffany MANOLIS <tiffany.mano@gmail.com>
2023-09-14 16:21:24 +0200

    GET request for username is FINALLY WORKING

--- App/src/components/settings_components/EditAvatar.tsx
+++ App/src/components/settings_components/EditAvatar.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import axios from "axios";
 
 const AvatarImage = styled.img`
   width: 60px;
@@ -10,12 +11,43 @@ const AvatarImage = styled.img`
 
 const userImageSrc = "../../public/icon/Avatar.svg";
 
+const BASE_URL = "http://localhost:3000";
+
+// async function getUser() {
+
+// 	const response = await fetch(`http://localhost:3000/user/me`);
+// 	const data = await response.json()
+// 							   .then( (data) => { return data;})
+// 							   .catch((error) => { console.error(error)});
+// 	const users = data;
+
+// 	console.log("users, ", users);
+// 	return users;
+// }
+
+const getUser = async () => {
+	const response = await axios.get(`http://localhost:3000/user/me`, { withCredentials: true })
+	return response.data;
+}	
+
 const EditAvatar: React.FC = () => {
+
+	const [username, setUsername] = useState<string>("");
+
+	useEffect( () => {
+		getUser().then(data => {
+			console.log(data);
+			setUsername(data.userName);
+		});
+	
+		// console.log(" user: user", user);
+	
+	}, [username]);
 
 	return (
 		<>
 		<AvatarImage src={userImageSrc} alt="User Avatar" />
-		<span>Username</span>
+		<span>{username}</span>
 		<button>Edit Avatar</button>
 		</>
 	);
