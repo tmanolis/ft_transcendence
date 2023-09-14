882a484bf99c6641512db8448363655d8019ccd3
Tiffany MANOLIS <tiffany.mano@gmail.com>
2023-09-14 14:26:53 +0200

    working on changing nickname

--- App/src/components/settings_components/EditUsername.tsx
+++ App/src/components/settings_components/EditUsername.tsx
@@ -1,17 +1,28 @@
 import React, { useState } from "react";
 
+interface FormEvent extends React.FormEvent<HTMLFormElement> {}
+
 const EditUsername: React.FC = () => {
 
 	const [newUsername, setnewUsername] = useState("");
+	
+	function handleSubmit(e: FormEvent) {
+		e.preventDefault();
+
+		setnewUsername("");
+	}
+
 	return (
 		<>
+		<form onSubmit={handleSubmit}>
 			<input
 				value={newUsername}
 				onChange={e => setnewUsername(e.target.value)} 
 				type ="text" id="new_nickname" 
 				placeholder="new nickname"
 			/>
-			<button className="basic_btn">Confirm</button>
+			<button type="submit" className="basic_btn">Confirm</button>
+		</form>
 		</>
 	);
 };
