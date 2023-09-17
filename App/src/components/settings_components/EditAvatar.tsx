730a2ad1ee0d7cce55eb842b208b2af0a6e2eb33
Tiffany Manolis <tiffany.mano@gmail.com>
2023-09-17 20:09:04 +0200

    upload and change avatar in progress

--- App/src/components/settings_components/EditAvatar.tsx
+++ App/src/components/settings_components/EditAvatar.tsx
@@ -9,8 +9,6 @@ const AvatarImage = styled.img`
   object-fit: cover;
 `;
 
-// const userImageSrc = "../../public/img/defaultAvatar.jpg";
-
 const BASE_URL = "http://localhost:3000";
 
 const getUser = async () => {
@@ -30,7 +28,7 @@ const EditAvatar: React.FC = () => {
 			setAvatarPath(data.avatar);})
 		.catch(error => {console.log(error)});
 	
-	}, [username]);
+	}, [username, avatarPath]);
 
 	return (
 		<div>
@@ -43,4 +41,89 @@ const EditAvatar: React.FC = () => {
 };
 
 export default EditAvatar
+
+// import React, { useEffect, useState } from "react";
+// import styled from "styled-components";
+// import axios from "axios";
+
+// const AvatarImage = styled.img`
+//   width: 70px;
+//   height: 70px;
+//   border-radius: 50%;
+//   object-fit: cover;
+// `;
+
+// const BASE_URL = "http://localhost:3000";
+
+// const getUser = async () => {
+//   const response = await axios.get(`${BASE_URL}/user/me`, { withCredentials: true });
+//   return response.data;
+// };
+
+
+// const EditAvatar: React.FC = () => {
+//   const [username, setUsername] = useState<string>("");
+//   const [avatarPath, setAvatarPath] = useState<string>("");
+//   const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+//   useEffect(() => {
+//     getUser()
+//       .then((data) => {
+//         setUsername(data.userName);
+//         setAvatarPath(data.avatar);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   }, [username, avatarPath]);
+
+//   const handleUpload = async () => {
+// 	if (selectedFile) {
+// 	  const formData = new FormData();
+// 	  formData.append("avatar", selectedFile);
+
+// 	  try {
+// 		const response = await axios.post(`${BASE_URL}/user/upload-avatar`, formData, {
+// 		  withCredentials: true,
+// 		  headers: {
+// 			"Content-Type": "multipart/form-data",
+// 		  },
+// 		});
+
+// 		// Update the avatarPath with the new avatar URL
+// 		setAvatarPath(response.data.avatar);
+// 	  } catch (error) {
+// 		console.error("Error uploading avatar:", error);
+// 	  }
+// 	}
+//   };
+
+//   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     const file = e.target.files?.[0];
+//     if (file) {
+//       setSelectedFile(file);
+//     }
+// 	console.log(file);
+// 	// handleUpload();
+//   };
+
+//   return (
+//     <div>
+//       <AvatarImage src={`data:image/png;base64,${avatarPath}`} alt="User Avatar" />
+//       <span>{username}</span>
+//       <input
+//         type="file"
+//         accept="image/*"
+//         onChange={handleFileChange}
+//         style={{ display: "none" }}
+//         ref={(input) => input && (input.value = "")}
+//       />
+//       <button onClick={() => document.querySelector("input[type='file']")?.click()}>Choose Avatar</button>
+//       <button onClick={handleUpload}>Upload Avatar</button>
+//     </div>
+//   );
+// };
+
+// export default EditAvatar;
+
 
--- App/src/components/settings_components/EditUsername.tsx
+++ App/src/components/settings_components/EditUsername.tsx
@@ -1,62 +1,6 @@
 import axios, { AxiosError } from "axios";
 import ConfirmButton from "./styles/ConfirmButton.styled";
 
-// const EditUsername: React.FC = (updateError) => {
-
-// 	const handleSubmit = async (e: React.FormEvent) => {
-// 		e.preventDefault();
-// 		setUpdateError("");
-
-// 		const form = e.target as HTMLFormElement;;
-// 		const formData = new FormData(form);
-// 		const new_username = formData.get("new_username");
-
-// 		form.reset();
-
-// 		const updateDTO = {
-// 		  userName: new_username,
-// 		};
-
-// 		try {
-// 			const response = await axios.patch("http://localhost:3000/user/update",
-// 			updateDTO,
-// 			{ withCredentials: true });
-// 			console.log(response);
-// 			window.location.reload();
-// 		  } catch (error) {
-// 			  handleUpdateError(error as AxiosError);
-// 		  }
-// 	}
-
-// 	const handleUpdateError = (error: AxiosError) => {
-//     if (error.response) {
-//       const status = error.response.status;
-//       if (status === 500) {
-//         setUpdateError("Update failed. Please try again!");
-//       }
-//     } else {
-//       setUpdateError("Network error occured");
-//     }
-// 	}
-
-// 	return (
-// 		<>
-// 			<form onSubmit={handleSubmit}>
-// 				<input
-// 					name = "new_username"
-// 					type ="text"
-// 					id="new_username" 
-// 					placeholder="new username"
-// 				/>
-// 				<ConfirmButton type="submit">Confirm</ConfirmButton>
-// 			</form>
-// 			{updateError && <div style={{ color: 'red', fontSize: '12px' }}>{updateError}</div>}
-// 		</>
-// 	);
-// };
-
-// export default EditUsername
-
 interface EditUsernameProps {
   onError: (error: string) => void;
 }
@@ -65,7 +9,6 @@ const EditUsername: React.FC<EditUsernameProps> = ({ onError }) => {
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    // onError(""); // Reset the error when submitting
 
     const form = e.target as HTMLFormElement;
     const formData = new FormData(form);
