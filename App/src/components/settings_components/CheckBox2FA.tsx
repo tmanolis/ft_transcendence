cd7860aead94c1afd7904af26f17945b6f2179d7
Tiffany Manolis <tiffany.mano@gmail.com>
2023-09-19 21:13:27 +0200

    WIP for enable 2FA

--- App/src/components/settings_components/CheckBox2FA.tsx
+++ App/src/components/settings_components/CheckBox2FA.tsx
@@ -1,9 +1,55 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:3000";
 
 const CheckBox2FA: React.FC = () => {
+  const [isChecked, setIsChecked] = useState(false);
+  
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.get(`${BASE_URL}/user/me`, {
+          withCredentials: true,
+        });
+        console.log("response: " + response.data.twoFAActivated);
+        setIsChecked(response.data.twoFAActivated);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  const handleCheckboxChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsChecked(event.target.checked);
+    console.log("state: " + event.target.checked);
+    
+    const updateDTO = {
+      twoFAActivated: event.target.checked
+    };
+
+    try {
+      const response = await axios.patch(
+        "http://localhost:3000/user/update",
+        updateDTO,
+        { withCredentials: true }
+      );
+      console.log(response);
+    } catch (error) {
+      console.error(error);;
+    }
+
+  };
+
   return (
     <>
-      <input type="checkbox" id="2fa_checkbox" />
+      <input type="checkbox"
+      id="2fa_checkbox"
+      checked={isChecked}
+      onChange={handleCheckboxChange} 
+      />
       <label htmlFor="2fa_checkbox">enable_2fa</label>
       <p>double factor authentification for maximum security</p>
     </>
