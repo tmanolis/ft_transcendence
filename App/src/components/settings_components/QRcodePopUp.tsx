a984c1f1d3feb3b90aba7034f873eed597c9eaa4
Tiffany MANOLIS <tiffany.mano@gmail.com>
2023-09-20 20:13:47 +0200

    basic structure to handle QR2FA

--- App/src/components/settings_components/QRcodePopUp.tsx
+++ App/src/components/settings_components/QRcodePopUp.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { QRCodePopupWrapper, QRCodeContainer } from './styles/QRCodePopUp.styled'; // Import the styled-components
+import React, { useState } from 'react'; // Import useState
+import { QRCodePopupWrapper, QRCodeContainer } from './styles/QRCodePopUp.styled';
 import ConfirmButton from './styles/ConfirmButton.styled';
 
 interface QRCodePopupProps {
@@ -7,14 +7,31 @@ interface QRCodePopupProps {
 }
 
 const QRCodePopup: React.FC<QRCodePopupProps> = ({ QRCode }) => {
+  const [inputValue, setInputValue] = useState(""); // State to hold input value
+
+  const handleCancelClick = () => {
+    window.location.reload();
+  };
+
+  const handleConfirmClick = () => {
+    console.log('Confirmed with input:', inputValue);
+    // request post to verify 2FA
+  };
+
   return (
     <QRCodePopupWrapper>
-    	<h3>Scan QRCode</h3>
-		<QRCodeContainer>
-			<img src={QRCode} alt="QRCode img" />
-		</QRCodeContainer>
-    <button>Cancel</button>
-    <ConfirmButton type="submit">Confirm</ConfirmButton>
+      <h3>Scan QRCode</h3>
+      <QRCodeContainer>
+        <img src={QRCode} alt="QRCode img" />
+      </QRCodeContainer>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)} // Update inputValue
+        placeholder="Enter code"
+      />
+      <button onClick={handleCancelClick}>Cancel</button>
+      <ConfirmButton type="submit" onClick={handleConfirmClick}>Confirm</ConfirmButton>
     </QRCodePopupWrapper>
   );
 };
