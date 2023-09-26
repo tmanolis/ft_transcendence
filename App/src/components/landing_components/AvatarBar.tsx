97ac8fc8f7a025e071eabf81c7c67c7ca884a240
Tiffany MANOLIS <tiffany.mano@gmail.com>
2023-09-26 20:54:40 +0200

    add logout request from avatarBar

--- App/src/components/landing_components/AvatarBar.tsx
+++ App/src/components/landing_components/AvatarBar.tsx
@@ -1,26 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import AvatarBarStyled from "./styles/AvatarBar.styled";
+import axios from "axios";
 
 type AvatarMenuProps = {
   to: string;
   buttonName: string;
+  onClick?: () => void; // Add optional onClick prop
 };
 
-const AvatarNavButton: React.FC<AvatarMenuProps> = ({ to, buttonName }) => {
+const AvatarNavButton: React.FC<AvatarMenuProps> = ({ to, buttonName, onClick }) => {
   return (
     <Link to={to}>
-      <h1>{buttonName}</h1>
+      <h1 onClick={onClick}>{buttonName}</h1>
     </Link>
   );
 };
 
 const AvatarBar: React.FC = () => {
+  
+  const handleLogout = async () => {
+    console.log("Logout button clicked");
+
+    try {
+      const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/auth/logout`, {
+        withCredentials: true,
+      });
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <AvatarBarStyled>
-      <AvatarNavButton to="/profile" buttonName="./Profile" />
-      <AvatarNavButton to="/settings" buttonName="./Settings" />
-      <AvatarNavButton to="/auth" buttonName="./Logout" />
+      <AvatarNavButton to="/profile" buttonName="Profile" />
+      <AvatarNavButton to="/settings" buttonName="Settings" />
+      <AvatarNavButton to="/auth" buttonName="Logout" onClick={handleLogout} />
     </AvatarBarStyled>
   );
 };
