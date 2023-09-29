4cf8f9e89e8bc3c49a7ed71ed635a2db8c38bf2b
Tiffany MANOLIS <tiffany.mano@gmail.com>
2023-09-29 20:09:17 +0200

    home page done and redirect to sign in

--- App/src/pages/Home.tsx
+++ App/src/pages/Home.tsx
@@ -1,12 +1,18 @@
+import { useNavigate } from "react-router-dom";
 import { HomeWrapper } from "../components/styles/HomeWrapper.styled";
 
 const Home = () => {
+  const navigate = useNavigate(); // Using useNavigate hook to handle navigation
   
+  const handlePlayClick = () => {
+    navigate("/auth"); // Navigating to the "/auth" route when the button is clicked
+  };
+
   return (
     <HomeWrapper>
       <h1>Pong Story Short <span>3.0</span></h1>
       <div></div>
-      <button>PLAY</button>
+      <button onClick={handlePlayClick}>PLAY</button>
     </HomeWrapper>
   );
 };
