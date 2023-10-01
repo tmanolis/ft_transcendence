006de4d419c905c725ead51519915fc51403c85c
Edoardo Giacomini <giacomini.ed@gmail.com>
2023-10-01 22:02:40 +0200

    history on progress

--- App/src/components/profile_components/MatchHistory.tsx
+++ App/src/components/profile_components/MatchHistory.tsx
@@ -1,1 +1,36 @@
-// import MatchHistoryStyled from "./MatchHistory.styled";
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { MatchHistoryStyled, HistoryScrollingList, MatchElement } from './styles/MatchHistory.styled';
+
+const MatchHistory: React.FC = () => {
+
+  return (
+    <MatchHistoryStyled>
+        <h1>Match History</h1>
+        <HistoryScrollingList>
+            <MatchElement></MatchElement>
+            <MatchElement></MatchElement>
+            <MatchElement></MatchElement>
+            <MatchElement></MatchElement>
+            <MatchElement></MatchElement>
+            <MatchElement></MatchElement>
+            <MatchElement></MatchElement>
+            <MatchElement></MatchElement>
+            <MatchElement></MatchElement>
+            <MatchElement></MatchElement>
+            <MatchElement></MatchElement>
+            <MatchElement></MatchElement>
+            <MatchElement></MatchElement>
+            <MatchElement></MatchElement>
+            <MatchElement></MatchElement>
+            <MatchElement></MatchElement>
+            <MatchElement></MatchElement>
+            <MatchElement></MatchElement>
+            <MatchElement></MatchElement>
+            <MatchElement></MatchElement>
+        </HistoryScrollingList>
+    </MatchHistoryStyled>
+  );
+};
+
+export default MatchHistory;
--- App/src/components/profile_components/styles/MatchHistory.styled.tsx
+++ App/src/components/profile_components/styles/MatchHistory.styled.tsx
@@ -1,2 +1,40 @@
-// import React from "react";
-// import styled from "styled-components";
+import styled from "styled-components";
+
+export const MatchHistoryStyled = styled.div`
+	width: 317px;
+    height: 417px;
+    border: 1px solid #FFF;
+    background: #000;
+    box-shadow: 1px 2px 1px 0px rgba(255, 255, 255, 0.90);
+
+	h1{
+        margin: 0;
+        padding-left: 5%;
+        color: #FFF;
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 500;
+        line-height: 70px; /* 350% */
+        letter-spacing: 0.4px;
+	}
+`;
+
+export const HistoryScrollingList = styled.div`
+  width: 100%;
+  height: 80%;
+  overflow-y: auto; /* Add a vertical scrollbar when content overflows */
+  padding: 5px; /* Add some padding to keep content away from the container edges */
+  box-sizing: border-box; /* Ensure padding is included in the container's dimensions */
+`;
+
+export const MatchElement = styled.div`
+  width: 285px;
+  height: 40px;
+  flex-shrink: 0;
+  border-top: 1px solid #FFF;
+  border-left: 1px solid #FFF;
+  background: #000;
+  box-shadow: 2px 2px 0px 0px rgba(157, 157, 157, 0.25);
+  margin-bottom: 5px; /* Add margin to separate MatchElement components */
+`;
+
