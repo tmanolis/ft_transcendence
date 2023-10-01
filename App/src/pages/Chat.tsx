f9c04281068c2a014d68ed588cd0936e0322d3c2
Tiffany Manolis <tiffany.mano@gmail.com>
2023-10-01 17:18:34 +0200

    basic page and routing set

---
+++ App/src/pages/Chat.tsx
@@ -0,0 +1,15 @@
+import PageContainer from "../components/PageContainer";
+import Landing from "../pages/Landing";
+
+const Chat = () => {
+  return (
+    <>
+      <Landing />
+      <PageContainer type="other">
+        
+      </PageContainer>
+    </>
+  )
+};
+
+export default Chat;
