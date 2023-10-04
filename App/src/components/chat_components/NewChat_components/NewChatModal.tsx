722357dbbc02b767e0111196be52a9b06283cb76
Tiffany MANOLIS <tiffany.mano@gmail.com>
2023-10-04 20:51:01 +0200

    remove unused variable

--- App/src/components/chat_components/NewChat_components/NewChatModal.tsx
+++ App/src/components/chat_components/NewChat_components/NewChatModal.tsx
@@ -3,7 +3,6 @@ import { ModalContainer, PopUpWrapper } from "./styles/NewChatModal.styled";
 import iconSrc from "../../../../public/icon/Cross.svg";
 import { Socket } from "socket.io-client";
 import JoinChannel from "./JoinChannel";
-import ConfirmButton from "../../settings_components/styles/ConfirmButton.styled";
 import JoinDirectMessage from "./JoinDirectMessage";
 
 interface NewChatModalProps {
