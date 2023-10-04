ecae9e304488f24221068f7917489e481aae8958
Tiffany MANOLIS <tiffany.mano@gmail.com>
2023-10-04 19:59:19 +0200

    change navigate

--- App/src/pages/Verify2FA.tsx
+++ App/src/pages/Verify2FA.tsx
@@ -44,7 +44,7 @@ const Verify2FA = () => {
 		{ withCredentials: true }
 		);
 		console.log(response);
-    navigate("/");
+    	navigate("/landing");
 	} catch (error) {
 			handleUpdateError(error as AxiosError);
 	}
