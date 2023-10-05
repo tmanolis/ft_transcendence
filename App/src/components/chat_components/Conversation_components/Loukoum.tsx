c7da4517b07f2cc8c95ca3d6206e23505646b121
Tiffany MANOLIS <tiffany.mano@gmail.com>
2023-10-05 19:12:35 +0200

    message are fetch and updated but need to use ReactQuery and webSocket

--- App/src/components/chat_components/Conversation_components/Loukoum.tsx
+++ App/src/components/chat_components/Conversation_components/Loukoum.tsx
@@ -3,45 +3,56 @@ import React, { useState, useEffect } from 'react';
 import { ChatContainer, ChatListWrapper, Username } from './styles/Loukoum.styled';
 import { Room } from '../../../pages/Chat';
 interface Message {
-    id: number,
-	text: string,
-	sendtime: string, // DateTime
-	sender: string,
-	roomID: string
+  id: number,
+  text: string,
+  sendtime: string, // DateTime
+  sender: string,
+  roomID: string
 }
 
 interface LoukoumProps {
 	chatRoom: Room;
   }
 
-const Loukoum: React.FC<LoukoumProps> = ({ chatRoom }) => {
-  const [messagesList, setMessagesList] = useState<Message[]>([]);
-
-  const getMessagesList = async () => {
-	const nameRefacto = chatRoom.name.replace(/#/g, '%23');
-
-	try {
-		const response = await axios.get(
-		`${import.meta.env.VITE_BACKEND_URL}/channel/history?name=${nameRefacto}`,
-		{ withCredentials: true },
-		);
-		console.log(response);
-		setMessagesList(response.data.messages);
-	} catch (error) {
-		console.log(error);
-	}
-	};
-
-  useEffect(() => {
-    getMessagesList();
-  }, []); // Add an empty dependency array to run this effect only once
-
-  return (
-    <ChatListWrapper>
+  const Loukoum: React.FC<LoukoumProps> = ({ chatRoom }) => {
+    const [messagesList, setMessagesList] = useState<Message[]>([]);
+  
+    const getMessagesList = async () => {
+      const nameRefacto = chatRoom.name.replace(/#/g, '%23');
+  
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_BACKEND_URL}/channel/history?name=${nameRefacto}`,
+          { withCredentials: true }
+        );
+        setMessagesList(response.data.messages);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  
+    useEffect(() => {
+      // Fetch initial data
+      getMessagesList();
+  
+      // Polling interval in milliseconds (e.g., poll every 5 seconds)
+      const pollingInterval = 850;
+  
+      // Start polling
+      const intervalId = setInterval(() => {
+        getMessagesList();
+      }, pollingInterval);
+  
+      // Clean up interval when component is unmounted
+      return () => clearInterval(intervalId);
+    }, []); // Add an empty dependency array to run this effect only once
+  
+    return (
+      <ChatListWrapper>
         {Item(messagesList)}
-    </ChatListWrapper>
-  );
-}
+      </ChatListWrapper>
+    );
+  }
 
 export default Loukoum;
 
