04aa4604bbfb7a94fa1263c8e1cc976d823e1e89
EdoGi <giacomini.ed@gmail.com>
2023-10-06 03:40:47 +0200

    commit before dodo : pas dinfo

--- App/src/components/chat_components/Conversation_components/ConversationUserInfo.tsx
+++ App/src/components/chat_components/Conversation_components/ConversationUserInfo.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import axios from "axios"
 import { UserBannerStyled, UserDetails } from "./styles/ConversationUserInfo.styled";
 
 interface UserInfoProps {
@@ -8,9 +9,31 @@ interface UserInfoProps {
 	  isMuted: boolean;
 	  isBlocked: boolean;
 	};
-  }
+}
 
-  const UserInfo: React.FC<UserInfoProps> = ({ user }) => {
+const UserInfo: React.FC<UserInfoProps> = ({ user }) => {
+  const [avatarPath, setAvatarPath] = useState("");
+  const [status, setStatus] = useState("");
+
+  useEffect(() => {
+		const fetchUserData = async () => {
+			try {
+				if (user.userName) { // Check if versUsername is defined
+					const response = await axios.get(
+						`${import.meta.env.VITE_BACKEND_URL}/user/userByUsername?userName=${user.userName}`,
+						{
+							withCredentials: true,
+						}
+					);
+					setAvatarPath(response.data.avatar);
+          setStatus(response.data.status);
+				}
+			} catch (error) {
+				console.error(error);
+			}
+		};
+		fetchUserData();
+	}, [user]);
 
 	return (
 		<UserBannerStyled>
