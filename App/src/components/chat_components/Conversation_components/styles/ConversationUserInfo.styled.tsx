1ff2379c2a7fcf3aa50eb5cfa61ac4b4f37b4ef2
EdoGi <giacomini.ed@gmail.com>
2023-10-07 03:02:47 +0200

    component error img resolved

--- App/src/components/chat_components/Conversation_components/styles/ConversationUserInfo.styled.tsx
+++ App/src/components/chat_components/Conversation_components/styles/ConversationUserInfo.styled.tsx
@@ -90,7 +90,7 @@ const statusColors = {
   AWAY: "orange",
 };
 
-export const UserStatus = styled.div`
+export const UserStatus = styled.div<{ $userstatus?: string }>`
   display: flex;
   align-items: center; /* Align children vertically */
   font-size: 12px;
