e07eafe53fc38fc9dbf489db85e025ed2bfaa0a2
EdoGi <62963498+EdoGi@users.noreply.github.com>
2023-10-10 21:00:44 +0200

    Update MatchHistory.tsx

--- App/src/components/profile_components/MatchHistory.tsx
+++ App/src/components/profile_components/MatchHistory.tsx
@@ -35,7 +35,7 @@ const MatchHistory: React.FC<MatchHistoryProps> = ({ gameList, profileUser }) =>
         );
         setLoggedUserName(response.data.userName);
       } catch (error) {
-        console.log(error);
+        console.error(error);
       }
     };
 
