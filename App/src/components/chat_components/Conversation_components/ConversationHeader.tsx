159d9eb0e049a3ddf074927b65c1318609ad82ba
EdoGi <62963498+EdoGi@users.noreply.github.com>
2023-10-10 20:56:30 +0200

    Update ConversationHeader.tsx

--- App/src/components/chat_components/Conversation_components/ConversationHeader.tsx
+++ App/src/components/chat_components/Conversation_components/ConversationHeader.tsx
@@ -29,7 +29,7 @@ const ConversationHeader: React.FC<ConversationHeaderProps> = ({ chatRoom, roomN
         });
         setUsername(response.data.userName);
       } catch (error) {
-        console.log(error);
+        console.error(error);
       }
     };
 
